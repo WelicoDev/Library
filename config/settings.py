00cc83e32e2ac50c4564e8ec0ef6948fe02bd2b4
WelicoDev <130744978+WelicoDev@users.noreply.github.com>
2023-10-01 09:17:41 +0500

    gunicorn instaled

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%(%0+a%y+^-ah^)i$bp@^uc^2o^2cdgxfbk=@zs*4@249%8ngc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -140,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
